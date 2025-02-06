dd4df453c836a61a22d597d0fd92918e6fadfef2
Anand Sandilya <anand.sandilya@quantilope.com>
2025-02-06 14:40:13 +0100

    feat(monorepo): setup frontend

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,11 @@
-// import frontendConfig from "./frontend/eslint.config.mjs";
+import frontendConfig from "./frontend/eslint.config.mjs";
 import serverConfig from "./server/eslint.config.mjs";
 
 export default [
+  {
+    files: ["frontend/src/**/*.{ts,tsx}"],
+    ...frontendConfig,
+  },
   {
     files: ["server/**/*.ts"],
     ...serverConfig,
