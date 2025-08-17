ace2ea6bd900499969a6fa164efdaac4fedc60a6
Anand Sandilya <17026547+anand-san@users.noreply.github.com>
2025-08-17 20:09:07 +0200

    feat(server): place build file directly next to the frontend build

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,21 +9,18 @@ import { FullScreenLoader } from './components/loader';
 import { AppLayout } from './pages/Layout';
 
 const Home = lazy(() => import('./pages/Home'));
-
 function App() {
   return (
-    <>
-      <Suspense fallback={<FullScreenLoader />}>
-        <Router>
-          <Routes>
-            <Route path="/" element={<AppLayout />}>
-              <Route index element={<Home />} />
-            </Route>
-            <Route path="*" element={<Navigate to={'/'} />} />
-          </Routes>
-        </Router>
-      </Suspense>
-    </>
+    <Suspense fallback={<FullScreenLoader />}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<AppLayout />}>
+            <Route index element={<Home />} />
+          </Route>
+          <Route path="*" element={<Navigate to={'/'} />} />
+        </Routes>
+      </Router>
+    </Suspense>
   );
 }
 
